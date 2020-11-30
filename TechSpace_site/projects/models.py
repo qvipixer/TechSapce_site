db2a04afc704879ac3b2ee54b5a50527f20b0169
SerGG-PG <qvipixer@gmail.com>
2020-12-01 00:42:05 +0300

    upd_new_theme

--- TechSpace_site/projects/models.py
+++ TechSpace_site/projects/models.py
@@ -8,15 +8,14 @@ from home.models import Tags, SubCategory, Category
 # Create your models here.
 
 class Project(models.Model):  # Модель проектов
-    title = models.CharField('Название проекта', max_length=128)
+    title = models.CharField('Название проекта', max_length=14)
     category = models.ForeignKey(Category, related_name='project', on_delete=models.CASCADE)
-    subcategory = models.ForeignKey(SubCategory, related_name='project', on_delete=models.CASCADE)
+    category_sub = models.ForeignKey(SubCategory, related_name='project', on_delete=models.CASCADE)
     status = models.BooleanField('Статус', default=False)
     progress = models.CharField('Прогрес выполнения', max_length=255, default='')
     create = models.DateField('Дата добавления', default=datetime.date.today)
     update = models.DateTimeField('Дата редактирования', auto_now=True)
-    description_short = models.TextField('Короткое описание')
-    description_full = models.TextField('Полное описание')
+    description = models.TextField('Описание проекта')
     tags = models.ManyToManyField(Tags, blank=True, related_name='project')
     author = models.CharField('Автор проекта', max_length=64)
     image = models.ImageField('Эскиз', upload_to='project/%Y/%m/%d', blank=True, default='null.jpg')

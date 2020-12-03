3bc03dba67e01b8f819e78fbf0296624c3c357d0
SerGG-PG <qvipixer@gmail.com>
2020-12-04 02:22:00 +0300

    upd

--- TechSpace_site/tasks/views.py
+++ TechSpace_site/tasks/views.py
@@ -6,6 +6,10 @@ from tasks.models import Tasks
 # Create your views here.
 def tasks(request):
     tasks = Tasks.objects.order_by('status')  # сортировка в обратном порядке
+    tasks_true = Tasks.objects.filter(status='True')
+    tasks_false = Tasks.objects.filter(status='False')
     return render(request, 'tasks/tasks.html',
                   {'title': 'Список задач TechSpace',
-                   'tasks': tasks})
+                   'tasks': tasks,
+                   'tasks_true': tasks_true,
+                   'tasks_false': tasks_false})

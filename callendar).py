43cbe04a61d62a0360de7a8594a0df3abbf3d8ac
AdminS <qvipixer@gmail.com>
2021-09-21 23:34:22 +0300

    upd site

--- callendar).py
+++
@@ -1,5 +0,0 @@
-import calendar
-a = calendar.LocaleHTMLCalendar(locale='Russian_Russia')
-#with open('calendar.html', 'w') as g:
-#    print(a.formatyear(2020, width=3), file=g)
-print(a.formatyear(2020, width=3))
